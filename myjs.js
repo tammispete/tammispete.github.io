f9b0b5981440dd5c1417f2e8bc5eba1e8437e84e
tammispete <73185604+tammispete@users.noreply.github.com>
2021-10-21 23:40:35 +0300

    close lisätty takaisin

--- myjs.js
+++ myjs.js
@@ -7,6 +7,15 @@ list.addEventListener('click', function (ev) {
     }
 }, false);
 
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
 // lisätään listaan uusi kohta
 function newElement() {
     var todoli = document.createElement("li");
